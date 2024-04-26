724a92af2c8b0bceeac8052e2937dff6060d88ec
Shashi-1904 <shashikantpawase71@gmail.com>
2024-04-27 00:36:56 +0530

    First Commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const app = require('./config/express');
+const config = require('./config/config')
+const express=require('express')
+const path=require('path')
+
+// initialize mongo
+require('./config/mongoose');
+
+app.use(express.json());
+
+const _dirname=path.dirname("")
+
+const buildpath = path.join(_dirname,"../build")
+
+app.use(express.static(buildpath));
+
+
+app.listen(config.port, () => {
+    console.log(`listening on port ${config.port} (${config.env})`);
+});
+
